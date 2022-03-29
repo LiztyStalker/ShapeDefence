ac00aa9599d20cf3f3ca4dd3714ca7f2174333fe
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2022-03-29 15:50:37 +0900

    NEW : GoogleSheet 적용 및 Sheet 불러오기 테스트 완료

--- Assets/Scripts/Utility/DataGenerator/GoogleSheetGenerator.cs
+++ Assets/Scripts/Utility/DataGenerator/GoogleSheetGenerator.cs
@@ -5,205 +5,204 @@ namespace Utility.Generator
     using UnityEngine;
     using UnityEditor;
     using ScriptableObjectData;
-    //using GoogleSheetsToUnity;
+    using GoogleSheetsToUnity;
     using System.Linq;
-    //using Utility.Data;
-    //using LitJson;
+    using LitJson;
 
     public class GoogleSheetGenerator
     {
 
-        //#region ##### CreateAndUpdate #####
+        #region ##### CreateAndUpdate #####
 
         public static void CreateAndUpdateTextAsset(string sheetkey, string worksheet, string dataPath, string bundleName = null)
         {
-        //    GSTU_Search gstuSearcher = new GSTU_Search(sheetkey, worksheet);
-        //    SpreadsheetManager.Read(gstuSearcher, sheet =>
-        //    {
-        //        OnCreateAndUpdateTextAssetEvent(sheet, worksheet, dataPath, bundleName, gstuSearcher.titleRow);
-        //    });
-        //}
+            GSTU_Search gstuSearcher = new GSTU_Search(sheetkey, worksheet);
+            SpreadsheetManager.Read(gstuSearcher, sheet =>
+            {
+                OnCreateAndUpdateTextAssetEvent(sheet, worksheet, dataPath, bundleName, gstuSearcher.titleRow);
+            });
+        }
 
 
-        //private static void OnCreateAndUpdateTextAssetEvent(GstuSpreadSheet sheet, string worksheet, string dataPath, string bundleName, int startRow)
-        //{
-        //    Dictionary<string, List<Dictionary<string, Dictionary<string, string>>>> _dic = new Dictionary<string, List<Dictionary<string, Dictionary<string, string>>>>(); 
+        private static void OnCreateAndUpdateTextAssetEvent(GstuSpreadSheet sheet, string worksheet, string dataPath, string bundleName, int startRow)
+        {
+            Dictionary<string, List<Dictionary<string, Dictionary<string, string>>>> _dic = new Dictionary<string, List<Dictionary<string, Dictionary<string, string>>>>();
 
-        //    string[] arr = new string[1];
+            string[] arr = new string[1];
 
-        //    string tmpkey = "";
+            string tmpkey = "";
 
-        //    //Structure
-        //    //{"data":
-        //    //    {"key" : Dictionary
-        //    //		[{"values" : List, Dictionary
-        //    //            {"Language_000 : 0}, Dictionary
-        //    //            { Language_001 : 0},						
-        //    //	  		  {"Language_000 : 0}, 
-        //    //            { Language_001 : 0}
-        //    //      }
-        //    //		],
-        //    //		[
-        //    //      ...
-        //    //      ]
-        //    //    }
-        //    //{
+            //Structure
+            //{"data":
+            //    {"key" : Dictionary
+            //		[{"values" : List, Dictionary
+            //            {"Language_000 : 0}, Dictionary
+            //            { Language_001 : 0},						
+            //	  		  {"Language_000 : 0}, 
+            //            { Language_001 : 0}
+            //      }
+            //		],
+            //		[
+            //      ...
+            //      ]
+            //    }
+            //{
 
-        //    ////첫줄은 헤더 및 키 라인
-        //    ///Key_2 Kor Eng
-        //    ////두번째 줄부터 데이터
+            ////첫줄은 헤더 및 키 라인
+            ///Key_2 Kor Eng
+            ////두번째 줄부터 데이터
 
-        //    var keys = sheet.rows.PrimaryValues.ToArray();
+            var keys = sheet.rows.PrimaryValues.ToArray();
 
-        //    for (int c = 0; c < keys.Length; c++)
-        //    {
-        //        var row = keys[c];
-        //        var key = row[0].value;
+            for (int c = 0; c < keys.Length; c++)
+            {
+                var row = keys[c];
+                var key = row[0].value;
 
-        //        if (!string.IsNullOrEmpty(key))
-        //        {
-        //            //첫라인이면 헤더 설정
-        //            if (c == 0)
-        //            {
-        //                //키라인을 제외하고 적용
-        //                arr = new string[row.Count - 1];
-        //                for(int i = 0; i < row.Count - 1; i++)
-        //                {
-        //                    arr[i] = row[i + 1].value;
-        //                }
-        //            }
-        //            //두번째부터 데이터라인
-        //            else
-        //            {
-        //                //key가 같으면
-        //                if(tmpkey == key)
-        //                {
-        //                    _dic[key].Add(new Dictionary<string, Dictionary<string, string>>());
+                if (!string.IsNullOrEmpty(key))
+                {
+                    //첫라인이면 헤더 설정
+                    if (c == 0)
+                    {
+                        //키라인을 제외하고 적용
+                        arr = new string[row.Count - 1];
+                        for (int i = 0; i < row.Count - 1; i++)
+                        {
+                            arr[i] = row[i + 1].value;
+                        }
+                    }
+                    //두번째부터 데이터라인
+                    else
+                    {
+                        //key가 같으면
+                        if (tmpkey == key)
+                        {
+                            _dic[key].Add(new Dictionary<string, Dictionary<string, string>>());
 
-        //                    _dic[key][_dic[key].Count - 1].Add("values", new Dictionary<string, string>());
+                            _dic[key][_dic[key].Count - 1].Add("values", new Dictionary<string, string>());
 
-        //                    for (int i = 0; i < row.Count - 1; i++)
-        //                    {
-        //                        _dic[key][_dic[key].Count - 1]["values"].Add(arr[i], row[i + 1].value);
-        //                    }
-        //                }
-        //                //key가 다르면
-        //                else
-        //                {
-        //                    if (!_dic.ContainsKey(key)) 
-        //                    {
-        //                        _dic.Add(key, new List<Dictionary<string, Dictionary<string, string>>>());
-        //                    }
+                            for (int i = 0; i < row.Count - 1; i++)
+                            {
+                                _dic[key][_dic[key].Count - 1]["values"].Add(arr[i], row[i + 1].value);
+                            }
+                        }
+                        //key가 다르면
+                        else
+                        {
+                            if (!_dic.ContainsKey(key))
+                            {
+                                _dic.Add(key, new List<Dictionary<string, Dictionary<string, string>>>());
+                            }
 
-        //                    _dic[key].Add(new Dictionary<string, Dictionary<string, string>>());
+                            _dic[key].Add(new Dictionary<string, Dictionary<string, string>>());
 
-        //                    _dic[key][_dic[key].Count - 1].Add("values", new Dictionary<string, string>());
+                            _dic[key][_dic[key].Count - 1].Add("values", new Dictionary<string, string>());
 
-        //                    for (int i = 0; i < row.Count - 1; i++)
-        //                    {
-        //                        Debug.Log(row[i + 1].value);
-        //                        _dic[key][_dic[key].Count - 1]["values"].Add(arr[i], row[i + 1].value);
-        //                    }
-        //                    tmpkey = key;
-        //                }
-        //            }
-        //        }                
-        //    }
+                            for (int i = 0; i < row.Count - 1; i++)
+                            {
+                                Debug.Log(row[i + 1].value);
+                                _dic[key][_dic[key].Count - 1]["values"].Add(arr[i], row[i + 1].value);
+                            }
+                            tmpkey = key;
+                        }
+                    }
+                }
+            }
 
-        //    Debug.Log(JsonMapper.ToJson(_dic));
+            Debug.Log(JsonMapper.ToJson(_dic));
 
-        //    TextAsset textAsset = new TextAsset(JsonMapper.ToJson(_dic));
+            TextAsset textAsset = new TextAsset(JsonMapper.ToJson(_dic));
 
-        //    System.IO.File.WriteAllText($"{dataPath}/{worksheet}.txt", textAsset.text);
+            System.IO.File.WriteAllText($"{dataPath}/{worksheet}.txt", textAsset.text);
 
-        //    AssetDatabase.Refresh();
+            AssetDatabase.Refresh();
 
-        //    //바로 적용되지 않음
-        //    UnityEditor.AssetImporter importer = UnityEditor.AssetImporter.GetAtPath($"{dataPath}/{worksheet}.txt");
-        //    importer.SetAssetBundleNameAndVariant(bundleName, "");
-        //    AssetDatabase.SaveAssets();
+            //바로 적용되지 않음
+            UnityEditor.AssetImporter importer = UnityEditor.AssetImporter.GetAtPath($"{dataPath}/{worksheet}.txt");
+            importer.SetAssetBundleNameAndVariant(bundleName, "");
+            AssetDatabase.SaveAssets();
 
             Debug.Log("Create And Update TextAsset End");
         }
 
 
 
-        ///// <summary>
-        ///// 생성 및 갱신
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="dataPath"></param>
-        ///// <param name="bundleName"></param>
+        /// <summary>
+        /// 생성 및 갱신
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataPath"></param>
+        /// <param name="bundleName"></param>
         public static void CreateAndUpdateAllData<T>(string sheetkey, string worksheet, string dataPath, string bundleName = null) where T : ScriptableObjectData
         {
-        //    GSTU_Search gstuSearcher = new GSTU_Search(sheetkey, worksheet, "A2", 2);
-        //    SpreadsheetManager.Read(gstuSearcher, sheet =>
-        //    { 
-        //        OnCreateAndUpdateEvent<T>(sheet, dataPath, bundleName, gstuSearcher.titleRow); 
-        //    });
-        //}
+            GSTU_Search gstuSearcher = new GSTU_Search(sheetkey, worksheet, "A2", 2);
+            SpreadsheetManager.Read(gstuSearcher, sheet =>
+            {
+                OnCreateAndUpdateEvent<T>(sheet, dataPath, bundleName, gstuSearcher.titleRow);
+            });
+        }
 
-        //private static void OnCreateAndUpdateEvent<T>(GstuSpreadSheet sheet, string dataPath, string bundleName, int startRow) where T : ScriptableObjectData
-        //{
-        //    T tmpData = null;
-        //    int index = 0;
+        private static void OnCreateAndUpdateEvent<T>(GstuSpreadSheet sheet, string dataPath, string bundleName, int startRow) where T : ScriptableObjectData
+        {
+            T tmpData = null;
+            int index = 0;
 
-        //    var keys = sheet.rows.PrimaryValues.ToArray();
+            var keys = sheet.rows.PrimaryValues.ToArray();
 
-        //    for (int c = 0; c < keys.Length; c++, index++)
-        //    {
-        //        var row = keys[c];
-        //        var key = row[0].value;
-        //        //var row = sheet.rows[key];
+            for (int c = 0; c < keys.Length; c++, index++)
+            {
+                var row = keys[c];
+                var key = row[0].value;
+                //var row = sheet.rows[key];
 
-        //        Debug.Log(row[0].value);
+                Debug.Log(row[0].value);
 
-        //        //연계 퀘스트 - 키가 같음
-        //        if (tmpData != null && tmpData.Key == key)
-        //        {
-        //            tmpData.AddData(row.Select(cell => cell.value).ToArray());
-        //            EditorUtility.SetDirty(tmpData);
-        //        }
-        //        else
-        //        {
-        //            tmpData = null;
-        //            //try
-        //            //{
-        //            var data = AssetDatabase.LoadAssetAtPath<T>($"{dataPath}/{typeof(T).Name}_{key}.asset");
+                //연계 퀘스트 - 키가 같음
+                if (tmpData != null && tmpData.Key == key)
+                {
+                    tmpData.AddData(row.Select(cell => cell.value).ToArray());
+                    EditorUtility.SetDirty(tmpData);
+                }
+                else
+                {
+                    tmpData = null;
+                    //try
+                    //{
+                    var data = AssetDatabase.LoadAssetAtPath<T>($"{dataPath}/{typeof(T).Name}_{key}.asset");
 
-        //            if (data == null)
-        //            {
-        //                data = ScriptableObject.CreateInstance<T>();
-        //                data.SetSortIndex(index);
-        //                data.SetData(row.Select(cell => cell.value).ToArray());
-        //                AssetDatabase.CreateAsset(data, $"{dataPath}/{typeof(T).Name}_{key}.asset");
-        //                data.SetAssetBundle(bundleName);
-        //                EditorUtility.SetDirty(data);
-        //                AssetDatabase.SaveAssets();
-        //            }
-        //            else
-        //            {
-        //                data.SetSortIndex(index);
-        //                data.SetData(row.Select(cell => cell.value).ToArray());
-        //                data.SetAssetBundle(bundleName);
-        //                EditorUtility.SetDirty(data);
-        //            }
+                    if (data == null)
+                    {
+                        data = ScriptableObject.CreateInstance<T>();
+                        data.SetSortIndex(index);
+                        data.SetData(row.Select(cell => cell.value).ToArray());
+                        AssetDatabase.CreateAsset(data, $"{dataPath}/{typeof(T).Name}_{key}.asset");
+                        data.SetAssetBundle(bundleName);
+                        EditorUtility.SetDirty(data);
+                        AssetDatabase.SaveAssets();
+                    }
+                    else
+                    {
+                        data.SetSortIndex(index);
+                        data.SetData(row.Select(cell => cell.value).ToArray());
+                        data.SetAssetBundle(bundleName);
+                        EditorUtility.SetDirty(data);
+                    }
 
-        //            tmpData = data;
-        //        }
-        //        //}
-        //        //catch (System.Exception e)
-        //        //{
-        //        //    Debug.LogWarning(e.Message);
-        //        //    AssetDatabase.DeleteAsset($"{dataPath}/{typeof(T).Name}_{key}.asset");
-        //        //}
-                
-                
-        //    }
+                    tmpData = data;
+                }
+                //}
+                //catch (System.Exception e)
+                //{
+                //    Debug.LogWarning(e.Message);
+                //    AssetDatabase.DeleteAsset($"{dataPath}/{typeof(T).Name}_{key}.asset");
+                //}
+
+
+            }
             Debug.Log("Create And Update End");
         }
 
-        //#endregion
+        #endregion
 
 
 
@@ -242,7 +241,7 @@ namespace Utility.Generator
         //        {
         //            var arr = list[i].GetDataArray();
 
-        //            for(int j = 0; j < arr.Length; j++)
+        //            for (int j = 0; j < arr.Length; j++)
         //            {
         //                saveList.Add(new List<string>(arr[j]));
         //            }
