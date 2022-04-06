359140e34a779cd9ee5bc2a235c0bb280ac7a877
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2022-04-06 14:45:27 +0900

    NEW : IDamagable IsDamagable

--- Assets/Scripts/Interface/Damagable/IDamagable.cs
+++ Assets/Scripts/Interface/Damagable/IDamagable.cs
@@ -3,6 +3,7 @@ namespace SDefence.Actor
     using SDefence.Attack;
     public interface IDamagable
     {
+        public bool IsDamagable { get; }
         public void SetDamage(IAttackUsableData data);
     }
 }
