54121eb84ccd651478f379604203f0d6b7c487f1
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2022-04-27 15:15:01 +0900

    NEW : IAssetUsableData Range

--- Assets/Scripts/Attack/Usable/IAttackUsableData.cs
+++ Assets/Scripts/Attack/Usable/IAttackUsableData.cs
@@ -3,6 +3,7 @@ namespace SDefence.Attack
     public interface IAttackUsableData
     {
         public float Delay { get; }
+        public float Range { get; }
         public bool IsZero { get; }
         public void Set(IAttackUsableData value);
         public void SetAttack(string startValue, string increaseValue, string increaseRate, int upgrade);
